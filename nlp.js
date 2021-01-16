2c9547a4d18cc70d90c9d65d1eeb5fb790acacdb
Shriram Holla <bab.shriram@gmail.com>
2021-01-16 00:20:02 -0500

    NLP code added

---
+++ nlp.js
@@ -0,0 +1,44 @@
+var async = require('async');
+
+
+async function detectFulltext(filePath, callback) {
+  
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Read a local image as a text document
+  const [result] = await client.documentTextDetection(filePath);
+  const fullTextAnnotation = result.fullTextAnnotation.text;
+  console.log(fullTextAnnotation);
+  callback(null, fullTextAnnotation);
+}
+
+async function NLP(str) {
+  // Imports the Google Cloud client library
+  const language = require('@google-cloud/language');
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  const document = {
+    "type":"PLAIN_TEXT",
+    "language": "EN",
+    "content": str
+  }
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeEntities({document: document, encodingType: "UTF8"});
+  console.log(result);
+}
+
+function imageToText(path) {
+  async.waterfall([
+    async.apply(detectFulltext, path),
+    NLP,
+  ]);
+}
+
+imageToText("h5.jpg");
